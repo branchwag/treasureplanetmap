0b33ccd640738f4d2f401f6338b284b21fb7093e
branchwag <branchleannelmb@gmail.com>
2024-10-16 17:59:24 -0400

    adds start of first ring around planet

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
@@ -15,8 +13,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-
-//renderer.render(scene, camera);
 
 //lights
 const pointLight = new THREE.PointLight(0xffffff, 1);
@@ -27,11 +23,24 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
 //shapes here
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial({ color: 0x00FF41, wireframe: true });
-const sphere = new THREE.Mesh(geometry, material);
+const planetGeometry = new THREE.SphereGeometry(15, 32, 16);
+const planetMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF41, wireframe: true });
+const planetSphere = new THREE.Mesh(planetGeometry, planetMaterial);
+scene.add(planetSphere);
 
-scene.add(sphere);
+const ringGeometry = new THREE.RingGeometry(20, 30, 32);
+const ringMaterial = new THREE.MeshBasicMaterial({
+  color: 0x00FF41,
+  side: THREE.DoubleSide,
+  wireframe: true
+});
+const ring = new THREE.Mesh(ringGeometry, ringMaterial);
+ring.rotation.x = Math.PI / 2;
+
+const planetGroup = new THREE.Group();
+planetGroup.add(planetSphere);
+planetGroup.add(ring);
+scene.add(planetGroup);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -55,8 +64,8 @@ Array(200).fill().forEach(addStar);
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.005;
+  planetGroup.rotation.x += 0.01;
+  planetGroup.rotation.y += 0.005;
 
   controls.update();
 
