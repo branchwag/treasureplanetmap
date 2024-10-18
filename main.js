b55c1c4a071969570f5b90cd509a02032b45cd0a
branchwag <branchleannelmb@gmail.com>
2024-10-18 19:13:11 -0400

    adds planet

--- main.js
+++ main.js
@@ -25,11 +25,15 @@ scene.add(ambientLight);
 //shapes here
 
 const moonSphere = new THREE.SphereGeometry(2, 10, 16);
-
 const planetGeometry = new THREE.SphereGeometry(10, 32, 16);
+const lilPlanetGeo = new THREE.SphereGeometry(12, 10, 16);
+
 const planetMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF41, wireframe: true });
 const planetSphere = new THREE.Mesh(planetGeometry, planetMaterial);
 scene.add(planetSphere);
+
+const lilPlanet = new THREE.Mesh(lilPlanetGeo, planetMaterial);
+scene.add(lilPlanet);
 
 const moon = new THREE.Mesh(moonSphere, planetMaterial);
 
@@ -52,6 +56,7 @@ ring.position.set = (0, 0, 0);
 ringTwo.position.set = (0, 0, 0);
 
 moon.position.set(50, 0, -30); //rightleft, updown, forward/backward
+lilPlanet.position.set(50, 0, -200);
 
 const planetGroup = new THREE.Group();
 planetGroup.add(planetSphere);
@@ -60,6 +65,7 @@ planetGroup.add(ringTwo);
 scene.add(planetGroup);
 
 scene.add(moon);
+scene.add(lilPlanet);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -89,6 +95,7 @@ function animate() {
   moon.position.x = Math.cos(time * 0.05) * 50;
   moon.position.z = Math.sin(time * 0.05) * 50;
 
+  lilPlanet.rotation.y += 0.005;
 
   controls.update();
 
